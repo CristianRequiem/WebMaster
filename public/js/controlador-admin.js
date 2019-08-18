c25c2be0302c6c86f2962fe5a81b33bbdcbe15ec
CristianRequiem <cristianrequiem@yahoo.es>
2019-08-17 20:54:47 -0600

    Updates

--- public/js/controlador-admin.js
+++ public/js/controlador-admin.js
@@ -1,4 +1,5 @@
-function generarArchivos(){
+//Función para generar la sección de pantalla de Archivos en Admin
+function generarSeccionArchivos(){
     document.getElementById('titulo').innerHTML = 'Archivos';
     //document.getElementsById('cuerpo').innerHTML = '';
     document.getElementById('cuerpo').innerHTML =
@@ -24,20 +25,20 @@ function generarArchivos(){
                     </select>
                 </div>
                 <div class="row mt-4">
-                    <button type="button" class="btn btn-primary mx-auto">Guardar</button>
+                    <button type="button" class="btn btn-custom mx-auto">Guardar</button>
                 </div>
             </form>
         </div>
         <div class="col-6 mx-auto">
             <div class="row">
                 <div class="col-4">
-                    <button type="button" class="btn btn-success">Galería de Fotos</button>
+                    <button type="button" class="btn btn-custom">Galería de Fotos</button>
                 </div>
                 <div class="col-4">
-                    <button type="button" class="btn btn-success">Galería de Videos</button>
+                    <button type="button" class="btn btn-custom">Galería de Videos</button>
                 </div>
                 <div class="col-4">
-                    <button type="button" class="btn btn-success">Galería de Archivos</button>
+                    <button type="button" class="btn btn-custom">Galería de Archivos</button>
                 </div>
             </div>
         </div>
@@ -46,7 +47,8 @@ function generarArchivos(){
     ;
 }
 
-function generarComentarios(){
+//Función para generar la sección de pantalla de Comentarios en Admin
+function generarSeccionComentarios(){
     document.getElementById('titulo').innerHTML = 'Comentarios';
     //document.getElementById('cuerpo').innerHTML = '';
     document.getElementById('cuerpo').innerHTML = 
@@ -87,7 +89,8 @@ function generarComentarios(){
     ;
 }
 
-function generarUsuarios(){
+//Función para generar la sección de pantalla de Usuarios en Admin
+function generarSeccionUsuarios(){
     document.getElementById('titulo').innerHTML = 'Usuarios';
     document.getElementById('cuerpo').innerHTML = 
     `
@@ -95,8 +98,8 @@ function generarUsuarios(){
         <div class="col-4 mx-auto">
             <table class="table table-borderless table-hover">
                 <tr>
-                    <td><a href="#" class="btn btn-success" style="width:200px;" data-toggle="modal" data-target="#modalCrearUsuario">Crear usuario</a></td>
-                    <td><a href="#" class="btn btn-success" style="width:200px;" data-toggle="modal" data-target="#modalCrearRol">Crear rol</a></td>
+                    <td><a href="#" class="btn btn-custom" style="width:200px;" data-toggle="modal" data-target="#modalCrearUsuario">Crear usuario</a></td>
+                    <td><a href="#" class="btn btn-custom" style="width:200px;" data-toggle="modal" data-target="#modalCrearRol">Crear rol</a></td>
                 </tr>
             </table>
         </div>
@@ -121,7 +124,7 @@ function generarUsuarios(){
                         <td>johndoe@yahoo.com</td>
                         <td>1234</td>
                         <td>Genérico</td>
-                        <td><a href="#" class="btn btn-dark" title="Editar" style="font-size:10px;"><span class="fas fa-user-edit"></span></a></td>
+                        <td><a href="#" class="btn btn-success" title="Editar" style="font-size:10px;"><span class="fas fa-user-edit"></span></a></td>
                     </tr>
                 </tbody>
             </table>
@@ -130,14 +133,17 @@ function generarUsuarios(){
     `;
 }
 
-function generarEntradas(){
+//Función para generar la sección de pantalla de Entradas/Posts en Admin
+function generarSeccionEntradas(){
     document.getElementById('titulo').innerHTML = 'Entradas/Posts';
     document.getElementById('cuerpo').innerHTML = 
     `
-    <div class="row ml-5 mt-2">
-        <a href="#" class="btn btn-success" data-toggle="modal" data-target="#modalCrearEntrada">Crear entrada</a>
+    <div class="row mt-2">
+        <div class="mx-auto">
+            <a href="#" class="btn btn-custom" data-toggle="modal" data-target="#modalCrearEntrada">Crear entrada</a>
+        </div>
     </div>
-    <div class="row mt-5">
+    <div class="row mt-2">
         <div class="col-12 ml-4">
             <table class="table table-borderless table-hover">
                 <thead>
@@ -155,7 +161,7 @@ function generarEntradas(){
                         <td>Categoría 1</td>
                         <td>
                             <a href="#" class="btn btn-info mx-2" title="Ver" style="font-size:10px;"><span class="fas fa-eye"></span></a>
-                            <a href="#" class="btn btn-dark mx-2" title="Editar" style="font-size:10px;"><span class="fas fa-edit"></span></a>
+                            <a href="#" class="btn btn-success mx-2" title="Editar" style="font-size:10px;"><span class="fas fa-edit"></span></a>
                             <a href="#" class="btn btn-danger mx-2" title="Eliminar" style="font-size:10px;"><span class="fas fa-trash-alt"></span></a>
                         </td>
                     </tr>
@@ -163,5 +169,73 @@ function generarEntradas(){
             </table>
         </div>
     </div>
+    `;
+}
+
+//Función para generar la sección de pantalla de Páginas en Admin
+function generarSeccionPaginas(){
+    document.getElementById('titulo').innerHTML = 'Páginas';
+    document.getElementById('cuerpo').innerHTML = 
+    `
+    <div class="row mt-2">
+        <div class="mx-auto">
+            <a href="#" class="btn btn-custom" data-toggle="modal" data-target="#modalCrearPagina">Crear página</a>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-12 col-md-6 mx-auto">
+            <table class="table table-borderless table-hover">
+                <thead>
+                    <td><b>Nombre de página</b></td>
+                    <td><b>Ver</b></td>
+                    <td><b>Editar</b></td>
+                    <td><b>Eliminar</b></td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Página 1</td>
+                        <td><a href="#" class="btn btn-info" title="Ver" style="font-size:10px;"><span class="fas fa-eye"></span></a></td>
+                        <td><a href="#" class="btn btn-success" title="Editar" style="font-size:10px;"><span class="fas fa-edit"></span></a></td>
+                        <td><a href="#" class="btn btn-danger" title="Eliminar" style="font-size:10px;"><span class="fas fa-trash-alt"></span></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    `;
+}
+
+//Función para generar la sección de pantalla de Temas en Admin
+function generarSeccionTemas(){
+    document.getElementById('titulo').innerHTML = 'Temas/Plantillas';
+    document.getElementById('cuerpo').innerHTML = 
+    `
+    <div class="row mt-2">
+        <div class="mx-auto">
+            <a href="#" class="btn btn-custom" data-toggle="modal" data-target="#modalCrearTema">Crear plantilla</a>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-12 col-md-6 mx-auto">
+            <table class="table table-borderless table-hover">
+                <thead>
+                    <td><b>Nombre del tema</b></td>
+                    <td><b>Descripción</b></td>
+                    <td><b>Ver</b></td>
+                    <td><b>Editar</b></td>
+                    <td><b>Eliminar</b></td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Plantilla 1</td>
+                        <td>Lorem ipsum</td>
+                        <td><a href="#" class="btn btn-info" title="Ver" style="font-size:10px;"><span class="fas fa-eye"></span></a></td>
+                        <td><a href="#" class="btn btn-success" title="Editar" style="font-size:10px;"><span class="fas fa-edit"></span></a></td>
+                        <td><a href="#" class="btn btn-danger" title="Eliminar" style="font-size:10px;"><span class="fas fa-trash-alt"></span></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div> 
     `;
 }
